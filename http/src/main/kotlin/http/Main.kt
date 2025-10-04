bdbe2ff7bf4aa2f6d538aa169416756dc2dd01e5
mihaillinkov <mihaillinkov@gmail.com>
2025-10-04 16:06:38 +0100

    Add better IO handling of inputstream. Improved parsing of request, including body and headers. Changed line break handling of http request

--- http/src/main/kotlin/http/Main.kt
+++ http/src/main/kotlin/http/Main.kt
@@ -7,5 +7,5 @@ private const val DEFAULT_PORT = 8080
 fun main(vararg args: String) = runBlocking {
     val port = args.getOrNull(0)?.toInt() ?: DEFAULT_PORT
 
-    App.run(Config(port, 2))
+    App(Config(port, 2, 2000)).run()
 }
--- http/src/main/kotlin/http/Response.kt
+++ http/src/main/kotlin/http/Response.kt
@@ -1,5 +1,9 @@
 package http
 
+import http.ResponseCode.REQUEST_TIMEOUT
+
+private const val PROTOCOL_VERSION = "HTTP/1.1"
+
 data class HttpResponse(
     val status: ResponseStatus,
     val body: String? = null,
@@ -8,15 +12,28 @@ data class HttpResponse(
 
 data class ResponseStatus(val code: Int, val message: String = "")
 
-fun buildResponse(response: HttpResponse) =
-    "${buildResponseFirstLine(response)}\n${response.headers.joinToString("\n")}\n${response.body}".toByteArray()
+fun buildHttpResponse(response: HttpResponse): ByteArray {
+    val headers = response.headers.joinToString("\n")
+    return """${buildResponseFirstLine(response)}
+        |$headers
+        |
+        |${response.body}
+    """.trimMargin().toByteArray()
+}
+
+fun timeoutResponse(): HttpResponse {
+    return HttpResponse(
+        status = ResponseStatus(REQUEST_TIMEOUT, "Request Timeout"),
+        headers = listOf("Connection: close"))
+}
 
 fun buildResponseFirstLine(response: HttpResponse): String {
-    return "HTTP/1.1 ${response.status.code} ${response.status.message}\nContent-Length: 0\n"
+    return "$PROTOCOL_VERSION ${response.status.code} ${response.status.message}"
 }
 
 object ResponseCode {
     const val OK = 200
     const val NOT_FOUND = 404
+    const val REQUEST_TIMEOUT = 408
     const val SERVER_ERROR = 500
 }
