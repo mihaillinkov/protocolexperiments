8bf66f726ad58660565652b1e0c119a973086802
mihaillinkov <mihaillinkov@gmail.com>
2025-10-11 16:07:18 +0100

    Add more response tests

--- http/src/test/kotlin/http/ResponseTests.kt
+++ http/src/test/kotlin/http/ResponseTests.kt
@@ -4,13 +4,32 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.beEmpty
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldBeEqualIgnoringCase
 
-class ResponseTests: FunSpec({
-    test("timeout response should have status 408") {
-        val response = timeoutResponse()
+class ResponseTests: FunSpec() {
+    init {
+        test("timeout response should have status 408") {
+            val response = timeoutResponse()
 
-        response.body shouldBe null
-        response.status shouldBe ResponseStatus(408, "Request Timeout")
-        response.headers should beEmpty()
+            response.body shouldBe null
+            response.status shouldBe ResponseStatus(408, "Request Timeout")
+            response.headers should beEmpty()
+        }
+
+        test("contentLengthHeader should return valid content-length header") {
+            contentLengthHeader(101) shouldBeEqualIgnoringCase "content-length: 101"
+        }
+
+        test("buildResponseStartLine should include message when not null") {
+            val startLine = buildResponseStartLine(ResponseStatus(200, "Ok"))
+
+            startLine shouldBe "HTTP/1.1 200 Ok"
+        }
+
+        test("buildResponseStartLine should not include message when null") {
+            val startLine = buildResponseStartLine(ResponseStatus(200, null))
+
+            startLine shouldBe "HTTP/1.1 200"
+        }
     }
-})
+}
