d0339ce2a4ccb4f3146a95fb31dab50e8d8add72
mihaillinkov <mihaillinkov@gmail.com>
2025-10-13 14:42:42 +0100

    Add buildRequestObject tests

--- http/src/test/kotlin/http/RequestTest.kt
+++ http/src/test/kotlin/http/RequestTest.kt
@@ -4,11 +4,14 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.mockk.unmockkStatic
 import kotlinx.coroutines.CancellationException
+import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousSocketChannel
+import java.nio.channels.CompletionHandler
 
 
 class RequestTest: FunSpec() {
@@ -50,6 +53,61 @@ class RequestTest: FunSpec() {
             coEvery { socketChannel.readAwait(any<Int>()) } returnsMany(output)
 
             readBody(socketChannel, 9) shouldBe "test data".toByteArray(Charsets.UTF_8)
+        }
+
+        test("buildRequestObject test") {
+            mockSocketChannelRead(socketChannel, "GET /test HTTP/1.1\r\n\r\n".toByteArray(Charsets.UTF_8))
+
+            val request = buildRequestObject(socketChannel)
+
+            request.method shouldBe RequestMethod.GET
+            request.url shouldBe "/test"
+            request.body shouldBe null
+            request.headers shouldBe emptyList()
+        }
+
+        test("buildRequestObject should throw CancellationException when stream ends") {
+            mockSocketChannelRead(socketChannel, "GET /test HTTP/1.1".toByteArray(Charsets.UTF_8))
+
+
+            shouldThrow<CancellationException> {
+                buildRequestObject(socketChannel)
+            }
+        }
+    }
+}
+
+fun mockSocketChannelRead(socketChannel: AsynchronousSocketChannel, bytes: ByteArray) {
+    val mockFunc = readMock(bytes)
+
+    every {
+        socketChannel.read(any<ByteBuffer>(), anyNullable<Any?>(), any<CompletionHandler<Int, Any?>>())
+    } answers { (_, scope) ->
+        val (args1, _, args3) = scope.args
+        val buffer = args1 as ByteBuffer
+        val handler = args3 as CompletionHandler<Int, Any?>
+
+        mockFunc(buffer, handler)
+    }
+}
+
+fun readMock(bytes: ByteArray): (ByteBuffer, CompletionHandler<Int, Any?>) -> Unit {
+    var offset = 0
+
+    return { buffer, completionHandler ->
+        val bufferSize = buffer.limit()
+
+        val bytesToAddSize = minOf(bufferSize, bytes.size - offset)
+
+        val bytesToAdd = bytes.sliceArray(offset until  offset + bytesToAddSize)
+        buffer.put(bytesToAdd)
+
+        offset += bytesToAddSize
+
+        if (bytesToAddSize == 0) {
+            completionHandler.completed(-1, null)
+        } else {
+            completionHandler.completed(bytesToAddSize, null)
         }
     }
 }
