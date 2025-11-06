4dced3e46303367784cd7d3e93959fbe4d461140
mihaillinkov <mihaillinkov@gmail.com>
2025-11-06 14:24:49 +0000

    Rename config env variables

--- http/src/main/kotlin/http/Config.kt
+++ http/src/main/kotlin/http/Config.kt
@@ -11,10 +11,10 @@ fun buildConfig(
     parallelRequestLimit: Int? = null,
     socketBacklogSize: Int? = null): Config {
 
-    val port = port ?: Integer.getInteger("port", DEFAULT_PORT)
-    val timeout = requestTimeoutInMs ?: Integer.getInteger("request-timeout", DEFAULT_REQUEST_TIMEOUT)
-    val parallelRequestLimit = parallelRequestLimit ?: Integer.getInteger("parallel-request-limit", DEFAULT_PARALLEL_REQUEST_LIMIT)
-    val socketBacklogSize = socketBacklogSize ?: Integer.getInteger("socket-backlog-limit", DEFAULT_SOCKET_BACKLOG_LIMIT)
+    val port = port ?: Integer.getInteger("PORT", DEFAULT_PORT)
+    val timeout = requestTimeoutInMs ?: Integer.getInteger("REQUEST_TIMEOUT", DEFAULT_REQUEST_TIMEOUT)
+    val parallelRequestLimit = parallelRequestLimit ?: Integer.getInteger("PARALLEL_REQUEST_LIMIT", DEFAULT_PARALLEL_REQUEST_LIMIT)
+    val socketBacklogSize = socketBacklogSize ?: Integer.getInteger("SOCKET_BACKLOG_LIMIT", DEFAULT_SOCKET_BACKLOG_LIMIT)
 
     return Config(
         port = port,
@@ -28,3 +28,7 @@ data class Config(
     val requestTimeoutMs: Long,
     val parallelRequestLimit: Int,
     val socketBacklogSize: Int)
+
+/*
+java -DPARALLEL_REQUEST_LIMIT=4 -Dcom.sun.management.jmxremote  -Dcom.sun.management.jmxremote.port=9010  -Dcom.sun.management.jmxremote.rmi.port=9010 -Dcom.sun.management.jmxremote.ssl=false  -Dcom.sun.management.jmxremote.authenticate=false  -Djava.rmi.server.hostnamame=192.168.1.200 -cp "./*" http.MainKt
+ */
